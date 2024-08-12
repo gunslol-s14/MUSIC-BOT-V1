32683a0f2bd423f4467950dabb81f56706b5ca90
S14 <158401687+gunslol-s14@users.noreply.github.com>
2024-08-12 12:25:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ownerID: 'YOUR_OWNER_ID',
+  ownerID: '1272239604560564234',
   token: process.env.TOKEN,
   embedColor: '#FF0000',
   geniusToken: '0CHr9jtPjR7EKraXUVv6__8ELmEwFeUwt5vYAhk-is_fGOq-10-epdMzESkMPUnf',
